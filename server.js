dca3d5037833c8212488eef488786f92b3f0ceca
Your Name <you@example.com>
2026-06-25 20:50:43 -0500

    fix server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { CosmosClient } = require('@azure/cosmos');
 const authRoutes = require("./routes/auth");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
